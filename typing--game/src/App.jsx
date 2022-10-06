b17b275a7444d225a70ef8a7a3f87b7e2d960dfb
OlgaMozejko <78202579+OlgaMozejko@users.noreply.github.com>
2022-10-06 14:35:01 +0200

    typing game completed

--- typing--game/src/App.jsx
+++ typing--game/src/App.jsx
@@ -1,36 +1,17 @@
 import './App.css'
 import React from 'react'
+import useGame from './useGame'
 
 function App() {
 
-  const textExample = 'The road to success is not smooth but fraught with hurdles, unpredictability, dangers, and risk. The high risk of failure, especially with new initiatives, can dishearten the most spirited of people. Talent is never enough. With few exceptions, the best players are the hard workers. It is rightly said that Champions are not born, they are made.'
+  const [textBoxRef, text, setText, words,
+        calculateWords, timer, setTimer,
+        buttonText, setButtonText, gameStart,
+        setGameStart, startGame] = useGame()
 
-  const [text, setText] = React.useState('')
-  const [words, setWords] = React.useState('')
-  const [timer, setTimer] = React.useState('')
-  const [gameStart, setGameStart] = React.useState(false)
+  const textExample = 'The road to success is not smooth but fraught with hurdles, unpredictability, dangers, and risk. The high risk of failure, especially with new initiatives, can dishearten the most spirited of people. Talent is never enough. With few exceptions, the best players are the hard workers. It is rightly said that Champions are not born, they are made. ⇡'
 
-  function hangleChange(e) {
-    const {value} = e.target
-    setText(value)
-  }
-
-  function calculateWords(text) {
-    const WordsArr = text.trim().split(' ')
-    const filteredWords = WordsArr.filter(word => { 
-      return(
-        word !== '' &&  word !== '.'  &&  word !== '!'  &&  word !== ',')})
-    return(
-      setWords(filteredWords.length)
-    )
-  }
-
-  function startGame() {
-    setTimer(5)
-    setWords('')
-    setText('')
-    setGameStart(true)
-  }
+  const [sample, setSample] = React.useState('... ⇣')
 
   React.useEffect(() => {
     if(gameStart) {
@@ -40,39 +21,53 @@ function App() {
       if(timer === 0) {
         clearTimeout(Timeout)
         calculateWords(text)
+        setButtonText("Let's Type!")
         setGameStart(false)
       }
     }
   }, [timer, gameStart])
+
+  const colors = {
+    color: gameStart ? '#FA7070' : '#000028'
+  }
+
+  function hangleChange(e) {
+    const {value} = e.target
+    setText(value)
+  }
+
+  function changeSample() {
+    setSample(prevSample => {
+      return(
+        prevSample === '... ⇣' ? prevSample = textExample : prevSample = '... ⇣'
+      )
+    })
+  }
 
   return (
     <div className="App">
 
       <h1 className='title'>How fast can you type?</h1>
 
-      <h4 className='text--display'>time: {timer}</h4>
+      <h4 className='text--display' style={colors}>time: {timer}</h4>
       
 
       <textarea className='text--area' 
+        ref={textBoxRef} 
         onChange={hangleChange} 
         value={text} 
         disabled={!gameStart}
       />
 
-<h4 className='text--display'>type this ⇣</h4>
-        {!gameStart && <p className='sample--text'>...</p>}
-        {gameStart && <p className='sample--text'>{textExample}</p>}
-
       <button className='button'
-       onClick={() => startGame()} 
+       onClick={() => startGame(30)} 
        disabled={gameStart}
-       >
-
-
-
-        Let's type!</button>
+       >{buttonText}</button>
 
       <h4 className='text--display'>words: {words}</h4>
+
+      <h4 className='text--display'>Don't know what to type? try this</h4>
+      <p className='sample--text' onClick={() => changeSample()}>{sample}</p>
 
     </div>
   )
